596692ac3cd50fdcc38d346c21abd36d566043a7
Carlos Eduardo Pereira Alves <carlos.alves@meta.com.br>
2021-05-29 20:55:22 -0300

    refactor: adjustments token for test

--- product_service/src/config/token.ts
+++ product_service/src/config/token.ts
@@ -1,4 +1,6 @@
 export default {
-  token_example:
+  token_example_success:
     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijp7InVzZXJfaWQiOiIyMzIzZGYzZmQiLCJ1c2VyX3JvbGUiOiJnZXJlbnRlIn0sImV4cCI6MzI0MzI0MjM0MjMsImlhdCI6MTUxNjIzOTAyMn0.CFZH3HYtY1EEODfhMWBsGmKq4sv_GmAwIxSg9jKhGwI',
+  token_example_fail:
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0Ijp7InVzZXJfaWQiOiIyMzIzZGYzZmQiLCJ1c2VyX3JvbGUiOiJhbnlfcm9sZSJ9LCJleHAiOjMyNDMyNDIzNDIzLCJpYXQiOjE1MTYyMzkwMjJ9.8KvuxVsmIVR5ysLENsAqVosjrGzrXCQlqzSmtQ5EU4o',
 }
