2c8a1d93a6e7d63f7ca646203b523baf8b5e3055
Carlos Eduardo Pereira Alves <carlos.alves@meta.com.br>
2021-05-29 21:42:47 -0300

    test: return all products paginate

---
+++ product_service/src/modules/Product/services/IndexProductService.spec.ts
@@ -0,0 +1,25 @@
+import FakesProductRepository from '@modules/Product/repositories/fakes/FakesProductRepository'
+import { CreateProductService, IndexProductService } from '.'
+import tokenExample from '@config/token'
+
+describe('Index Product', () => {
+  it('should be able to create a new product', async () => {
+    const fakeProductRepository = new FakesProductRepository()
+    const createProduct = new CreateProductService(fakeProductRepository)
+
+    const product = await createProduct.execute({
+      name: 'any_name',
+      value: 1000,
+      weight: 100,
+      seller: 'any_seller',
+      store: 'any_store',
+      product_code: 'any_product_code',
+      token: tokenExample.token_example_success,
+    })
+
+    const indexProduct = new IndexProductService(fakeProductRepository)
+    const index = await indexProduct.execute()
+
+    expect(index).toBeTruthy()
+  })
+})
