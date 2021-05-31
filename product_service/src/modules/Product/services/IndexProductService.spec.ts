a8f8e5e258a4966c6a3cf9a9cde1254d0910014e
Carlos Eduardo Pereira Alves <carlos.alves@meta.com.br>
2021-05-31 20:39:24 -0300

    test: add role verify

--- product_service/src/modules/Product/services/IndexProductService.spec.ts
+++ product_service/src/modules/Product/services/IndexProductService.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '@shared/error'
 import FakesProductRepository from '@modules/Product/repositories/fakes/FakesProductRepository'
 import { CreateProductService, IndexProductService } from '.'
 import tokenExample from '@config/token'
@@ -20,11 +21,60 @@ describe('Index Product', () => {
     const payload = {
       limit: 10,
       page: 1,
+      token: tokenExample.token_example_success,
     }
 
     const indexProduct = new IndexProductService(fakeProductRepository)
     const index = await indexProduct.execute(payload)
 
     expect(index).toBeTruthy()
+  })
+
+  it('should verify if role is not gerente', async () => {
+    const fakeProductRepository = new FakesProductRepository()
+    const createProduct = new CreateProductService(fakeProductRepository)
+    const indexProduct = new IndexProductService(fakeProductRepository)
+
+    const product = await createProduct.execute({
+      name: 'any_name',
+      value: 1000,
+      weight: 100,
+      seller: 'any_seller',
+      store: 'any_store',
+      product_code: 'any_product_code',
+      token: tokenExample.token_example_success,
+    })
+
+    const productDelete = indexProduct.execute({
+      token: tokenExample.token_example_fail,
+      id: product._id,
+    })
+
+    expect(productDelete).rejects.toEqual({
+      errorCode: 403,
+      errorMessage: "You don't have permission to access this feature",
+    })
+  })
+
+  it('should verify if role is not provider', async () => {
+    const fakeProductRepository = new FakesProductRepository()
+    const createProduct = new CreateProductService(fakeProductRepository)
+    const indexProduct = new IndexProductService(fakeProductRepository)
+
+    const product = await createProduct.execute({
+      name: 'any_name',
+      value: 1000,
+      weight: 100,
+      seller: 'any_seller',
+      store: 'any_store',
+      product_code: 'any_product_code',
+      token: tokenExample.token_example_success,
+    })
+
+    const productDelete = indexProduct.execute({
+      id: product._id,
+    })
+
+    expect(productDelete).rejects.toBeInstanceOf(AppError)
   })
 })
